30349f58be6e13d0f7c2b9693e9b175b3b51adcb
samhopskills <sam.hopskills@gmail.com>
2025-11-13 12:22:36 +0100

    Remove unnecessary condition

--- src/main.cs
+++ src/main.cs
@@ -57,7 +57,7 @@ class Program
                             break;
                         }
                     }
-                    if(execFound || Searched)
+                    if(execFound)
                         break;
                 }
                 if(!execFound)
